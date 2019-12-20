5b155c9c0e7a825f87fc425aeefbfb40441b1902
andrewtr218 <andrewtr218@gmail.com>
2019-12-20 07:32:18 -0600

    added basic file structure and set up a loop and css to initialize a blank table

---
+++ script.js
@@ -0,0 +1,7 @@
+let tablemain=""
+
+
+    for (let i = 0; i < 8; i++) {
+        tablemain += `<div class="row"><div class="col c1"></div><div class="col c2"><div class="col c3"></div></div></div>`;
+        // scoreStr += '<td>'+scores+'</td>'
+    };
