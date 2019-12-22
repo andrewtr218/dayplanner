0d2628710bd4b474171442dd5db1779d5a6c72a6
andrewtr218 <andrewtr218@gmail.com>
2019-12-22 04:12:58 -0600

    Iniliazed column structure without functionality

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let tablemain=""
+// document.getElementById("tableFrame").createElement("Table");
+// document.querySelector("Table");
 
 
-    for (let i = 0; i < 8; i++) {
-        tablemain += `<div class="row"><div class="col c1"></div><div class="col c2"><div class="col c3"></div></div></div>`;
-        // scoreStr += '<td>'+scores+'</td>'
-    };
+// for (let i = 0; i < 8; i++) {
+//         // table += `<div class="row"><div class="col c1">"${i}"</div><div class="col c2">"${i}"</div><div class="col c3">"${i}"</div></div><br>`;
+//         var row = createElement("tr").innerText(`this is row ${i + 1}`)
+//         tableJava.append(row)
+
+// };
+
+
+// document.getElementById("tableFrame").append(tableJava)
+
+var table = $('<table>');
+
+for(i=0; i<8; i++){
+    var row = $('<tr>').addClass('row' + i);
+        for(j=0; j<3; j++){
+            var col = $('<td>').addClass("col" + j).text("col " + j);
+            row.append(col);
+        };
+
+    table.append(row);
+};
+
+
+$('#tableFrame').append(table);
+
